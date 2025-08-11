c835f0fc0c3fdb2a191c0db73c2a9354a625521e
Lucas <diaslucas8822@gmail.com>
2025-08-10 22:06:29 -0300

    update

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
         host: "postgres-db", // <-- MUDANÇA CRÍTICA: Nome do serviço no docker-compose
         port: 5432, // Dentro da rede Docker, a porta é a padrão 5432
         user: "postgres",
-        password: "sua_senha_docker", // A mesma senha definida no docker-compose
+        password: "83782813", // A mesma senha definida no docker-compose
         database: "policia_api",
     },
     migrations: {
